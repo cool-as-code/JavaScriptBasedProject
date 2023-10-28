2cab8b420e17120771bfab70808f90ce00fa03e0
Ankit Verma <ankitv78389@gmail.com>
2023-10-29 00:21:48 +0530

    :sparkles:[feat][added Digital Business Card Project]

---
+++ Digital-Business-Card/src/components/About.jsx
@@ -0,0 +1,17 @@
+const About = () => {
+  return (
+    <>
+      <h3 className="about-title">About</h3>
+      <p className="about-content">
+        I`m a very ambitious front-end developer looking for a role in an
+        established IT company with the opportunity to work with the latest
+        technologies on challenging and diverse projects.
+        <br />
+        I`m quiet confident, naturally curious, and perpetually working on
+        improving my chops one design problem at a time.
+      </p>
+    </>
+  );
+};
+
+export default About;
---
+++ Digital-Business-Card/src/components/Footer.jsx
@@ -0,0 +1,28 @@
+const Footer = () => {
+  return (
+    <footer>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.facebook.com/ankit.cool.524"
+      >
+        <img className="img-facebook" src="images/Facebook-Icon.png" />
+      </a>
+      <a target="_blank" rel="noreferrer" href="https://github.com/Ankitv003">
+        <img className="img-github" src="./images/GitHub-Icon.png" />
+      </a>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.instagram.com/ankit_verma_03"
+      >
+        <img className="img-instagram" src="./images/Instagram-Icon.png" />
+      </a>
+      <a target="_blank" rel="noreferrer" href="https://twitter.com/coolan9">
+        <img className="img-twitter" src="/images/Twitter-Icon.png" />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
